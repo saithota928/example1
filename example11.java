bd4b061c85ef014e73f15a7b2b3b51f2451ee09b
susmitha <susmitha.t9642@gmail.com>
2024-08-03 18:36:44 +0530

    commit

---
+++ example11.java
@@ -0,0 +1,5 @@
+public class example11{
+    public static void main(String[] args){
+        System.out.println("Hello how are you doing all");
+    }
+}
---
+++ example12.java
@@ -0,0 +1,5 @@
+public class example12{
+    public static void main(String[] args){
+        System.out.println("Hello how are you");
+    }
+}
